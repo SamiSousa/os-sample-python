9cd4f692fad660bb8748a9d79ac461ef14543ad7
SamiSousa <ssshahin@bu.edu>
2018-03-29 22:19:30 -0400

    print all environment variables to web

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,14 @@
 from flask import Flask
+import os
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+	# return all environment variables
+	environment = "Hello World!\n"
+	for key, value in os.environ.items():
+		environment += str(key) + ":" str(value) + ";\n"
+    return environment
 
 if __name__ == "__main__":
     application.run()
