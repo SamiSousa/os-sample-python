c9f1f8c3ab7492dc86cb819fde6c02cdf03a6d45
SamiSousa <ssshahin@bu.edu>
2018-03-29 22:26:12 -0400

    print all environment variables to web

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ def hello():
 	# return all environment variables
 	environment = "Hello World!\n"
 	for key, value in os.environ.items():
-		environment += str(key) + ":" str(value) + ";\n"
+		environment += str(key) + ":" + str(value) + ";\n"
     return environment
 
 if __name__ == "__main__":
